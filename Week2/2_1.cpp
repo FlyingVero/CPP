f0a115595182251497ac9b2badcd58e08d8031bd
FlyingVero <cuzon364@gmail.com>
2026-03-17 22:29:07 +0900

    Week2

---
+++ Week2/2_1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("      _ _         _ _       _     _    \n");
+    printf("     | | |       | | |     | |   | |   \n");
+    printf("     | | |       | | |     | |___| |   \n");
+    printf("  _  | | |    _  | | |     |  ___  |   \n");
+    printf(" | |_| | |   | |_| | |     | |   | |   \n");
+    printf("  \\___/|_|    \\___/|_|     |_|   |_|   \n");
+
+    return 0;
+}
