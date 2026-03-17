c6a0d37d59eb64ac2f0c30fefe6db7e3821f87f1
FlyingVero <cuzon364@gmail.com>
2026-03-17 19:26:56 +0900

    week3

--- Week3/97_1.cpp
+++ Week3/97_1.cpp
@@ -6,9 +6,65 @@ int main()
     {
         for(int j = 1; j <= i; ++j)
         {
-            std::cout << j;
+            std::cout << j << " ";
         }
         std::cout << "\n";
     }
 
+}
+
+#include <iostream>
+int main()
+{
+    //2번
+    for(int i = 5; i >= 1; --i)
+    {
+        for(int j = i; j >= 1; --j)
+        {
+            std::cout << j << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+#include <iostream>
+int main()
+{
+    //3번
+    for(int i = 1; i <= 5; ++i)
+    {
+        for(int k = 1; k <= 5 - 1; ++k)
+        {
+            std::cout << " ";
+        }
+        for(int j = i; j >= 1; --j)
+        {
+            std::cout << j << " ";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
+
+// 4번
+#include <iostream>
+int main()
+{
+    for(int i = 1; i <= 5; ++i)
+    {
+        for(int k = 1; k <= 5 - i; ++k)
+        {
+            std::cout << " ";
+        }
+        for(int j = i; j >= 1; --j)
+        {
+            std::cout << j << " ";
+        }
+        for(int j = 2; j <= i; ++j)
+        {
+            std::cout << j << " ";
+        }
+        std::cout << "\n";
+    }
+    return 0;
 }
