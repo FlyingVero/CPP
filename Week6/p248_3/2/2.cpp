f6a7618674c3db08881e25be6b2ecc8d49fe813b
FlyingVero <cuzon364@gmail.com>
2026-04-07 22:46:45 +0900

    WEEK

---
+++ Week6/p248_3/2/2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct MyTime {
+    int h, m, s;
+};
+
+void addTime(MyTime t1, MyTime t2, MyTime& res) {
+    int total_s = t1.s + t2.s;
+    int total_m = t1.m + t2.m + (total_s / 60);
+
+    res.s = total_s % 60;
+    res.m = total_m % 60;
+    res.h = t1.h + t2.h + (total_m / 60);
+}
+
+int main() {
+    MyTime t1 = {1, 40, 50}, t2 = {2, 30, 20};
+    MyTime result;
+    addTime(t1, t2, result);
+    
+    cout << "방법 2 결과: " << result.h << "시 " << result.m << "분 " << result.s << "초" << endl;
+    return 0;
+}
